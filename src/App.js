625e58e219a20a40248e002abcc0c06b4a63a929
Jerry Pallath <jpallath@nyu.edu>
2018-08-26 20:37:43 -0400

    win win win

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Blocker from "./containers/blocker";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Blocker />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/blocker.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import robbers from "../assets/TwoRobbersBlocker.png";
+import styled from "styled-components";
+
+class Blocker extends Component {
+  render() {
+    return (
+      <Block className="Blocker">
+        <Anchored href="https://www.instagram.com/two_robbers/">
+          <Robbers src={robbers} />
+        </Anchored>
+      </Block>
+    );
+  }
+}
+
+const Block = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  background: #f4f3e9;
+  overflow: hidden;
+`;
+
+const Robbers = styled.img`
+  width: 75%;
+  @media (max-width: 450px) {
+    {
+        transform: scale(1.5) ;
+    }
+`;
+
+const Anchored = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &hover {
+    cursor: pointer;
+  }
+`;
+
+export default Blocker;
